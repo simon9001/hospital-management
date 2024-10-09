304f8776f819b911726f94d34d1851243562057a
simon9001 <simongatungo300@gmail.com>
2024-10-09 19:15:12 +0300

    corrections

--- Systemproject/Systemproject/deployment.py
+++ Systemproject/Systemproject/deployment.py
@@ -1,12 +1,11 @@
 import os
-from .settings import*
+from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS =[os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
-DEBUG=False
-
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -19,18 +18,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE ='whitenoise.storage.compressedManifestStaticFilesStorage'
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string= os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='):pair.split('=')[1]for pair in connection_string.split('')}
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
 
-DATABASES ={
-    'default':{
-        'Engine':'django.db.backends.postgresql',
-        'NAME': parameters ['dbname'],
-        'HOST': parameters ['host'],
-        'USER': parameters ['user'],
-        'PASSWORD': parameters ['password'],
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters['dbname'],
+        'HOST': parameters['host'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
     }
 }
