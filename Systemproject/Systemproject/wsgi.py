b5ce8fd5b1a9255a505cad0c2da24be89d21b9aa
simon9001 <simongatungo300@gmail.com>
2024-10-18 11:09:41 +0300

    nexty

--- Systemproject/Systemproject/wsgi.py
+++ Systemproject/Systemproject/wsgi.py
@@ -7,20 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-###import os
-
-#from django.core.wsgi import get_wsgi_application
-
-#settings_module = 'azure-project.deployment'if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
-
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-
-#application = get_wsgi_application()
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospitalmanagement.settings')
+settings_module = 'azure-project.deployment'if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
+
